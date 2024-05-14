85ae7d1f99f78dfa423c8689e723e71c4cfc963a
Kale Williams <klaeboy.williams@gmail.com>
2024-05-14 13:06:24 -0500

    Changed bacterial spot Treatment

--- treatment.sql
+++ treatment.sql
@@ -7,6 +7,6 @@ CREATE TABLE treatments(
 INSERT INTO treatments VALUES ('Mango Anthracnose','Spray copper fungicides on the flower regularly for it to develop resistance as it develops.');
 INSERT INTO treatments VALUES ('Mango Healthy','Tree seems to be healthy.');
 INSERT INTO treatments VALUES ('Mango Powdery Mildew','To prevent fungus growth regular pruning of developing stems will stop the growth.');
-INSERT INTO treatments VALUES ('Tomato Bacterial spot','Cannot be cured, best solution is to burn or bury plant to prevent infection of other plants');
+INSERT INTO treatments VALUES ('Tomato Bacterial spot','Cannot be cured, best solution is to burn or bury leaf to prevent infection of other parts');
 INSERT INTO treatments VALUES ('Tomato Leaf Mold','Remove all parts with mold growing then use copper based fungicides. If the plant is growing under cover, improve its ventilation.');
 INSERT INTO treatments VALUES ('Tomato healthy','Plant is healthy');
