5f92c65e5249ccf97e2100f0188a54f3dea679c4
Zodius57 <96610780+Zodius57@users.noreply.github.com>
2024-05-13 22:20:44 -0500

    Add files via upload
    First commit

---
+++ treatment.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS  treatments(
+          plantType text,
+          treatmentMethod text );
+        
+INSERT INTO treatments VALUES ('Mango Anthracnose','Spray copper fungicides on the flower regularly for it to develop resistance as it develops.');
+INSERT INTO treatments VALUES ('Mango Healthy','Tree seems to be healthy.');
+INSERT INTO treatments VALUES ('Mango Powdery Mildew','To prevent fungus growth regular pruning of developing stems will stop the growth.');
+INSERT INTO treatments VALUES ('Tomato Bacterial spot','Cannot be cured, best solution is to burn or bury plant to prevent infection of other plants');
+INSERT INTO treatments VALUES ('Tomato Leaf Mold','Remove all parts with mold growing then use copper based fungicides. If the plant is growing under cover, improve its ventilation.');
+INSERT INTO treatments VALUES ('Tomato healthy','Plant is healthy');
